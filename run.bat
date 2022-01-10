af30db6243307bccd75bfaf0c3b02c61f21c318a
polygon <me@parthiv.dev>
2022-01-10 14:51:40 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+::A quick way to run the tic tac toe program. Navigate to the file directory in command prompt, or whatever shell you use, and type the word run into the console (maybe ./run.bat) and you will run the program.
 @echo off
 cls
 echo The date today is %date%
