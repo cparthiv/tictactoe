fd18b6cc3fed03c2262500611821a32fc1c24aab
polygon <me@parthiv.dev>
2022-01-10 14:49:44 -0800

    finish tictactoe

--- main.py
+++ main.py
@@ -14,9 +14,13 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 import random
 import os
+# player names
 player1 = ''
 player2 = ''
 gameboard = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+numstring=["1","2","3","4","5","6","7","8","9"]
+# ways a player can win
+combinations = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
 def display():
     print('_____________')
@@ -27,35 +31,56 @@ def display():
 
 player1 = input("Player 1, enter your name.")
 player2 = input("Player 2, enter your name.")
-
+winner = ''
 turn = random.randint(1,2)
-move = 'not'
-for _ in range(1,10):
-    if turn == 1:
-        while move.isdigit() == False:
-            move = input(f"{player1}, enter an index from 1-9.")
-            if move.isdigit() == False:
-                print("Please enter an integer from 1-9")
-            elif move.isdigit() == True:
-                if gameboard[int(move)-1] != ' ':
-                    print("Please enter an index that does not have a character in it already.")
-                    move = 'not'
-        gameboard[int(move)-1] = 'O'
-        turn = 2
-        move = 'not'
-        os.system('cls')
-        display()
-    elif turn == 2:
-        while move.isdigit() == False:
-            move = input(f"{player2}, enter an index from 1-9.")
-            if move.isdigit() == False:
-                print("Please enter an integer from 1-9.")
-            elif move.isdigit() == True:
-                if gameboard[int(move)-1] != ' ':
-                    print("Please enter an index that does not have a character in it already.")
-                    move = 'not'
-        gameboard[int(move)-1] = 'X'
-        turn = 1
-        move = 'not'
-        os.system('cls')
-        display() 
+move = '100'
+for _ in numstring:
+    if winner == 'O':
+        print("The winner is player 1!")
+    elif winner == 'X':
+        print("The winner is player 2!")
+    elif winner == '':
+        if turn == 1:
+            while move not in numstring:
+                move = input(f"{player1}, enter an index from 1-9.")
+                if move not in numstring:
+                    print("Please enter an integer from 1-9")
+                elif move in numstring:
+                    if gameboard[int(move)-1] != ' ':
+                        print("Please enter an index that does not have a character in it already.")
+                        move = '100'
+            gameboard[int(move)-1] = 'O'
+            for c in combinations:
+                trueness = 0
+                for x in c:
+                    if gameboard[x] == 'O':
+                        trueness +=1
+                if trueness == 3:
+                    winner = 'O'
+                
+                    
+            turn = 2
+            move = '100'
+            os.system('cls')
+            display()
+        elif turn == 2:
+            while move not in numstring:
+                move = input(f"{player1}, enter an index from 1-9.")
+                if move not in numstring:
+                    print("Please enter an integer from 1-9")
+                elif move in numstring:
+                    if gameboard[int(move)-1] != ' ':
+                        print("Please enter an index that does not have a character in it already.")
+                        move = '100'
+            gameboard[int(move)-1] = 'X'
+            for c in combinations:
+                trueness = 0
+                for x in c:
+                    if gameboard[x] == 'X':
+                        trueness +=1
+                if trueness == 3:
+                    winner = 'X'
+            turn = 1
+            move = '100'
+            os.system('cls')
+            display() 
